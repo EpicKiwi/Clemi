14b7c4383057ea07c6991944ea3d44ec2168bd1f
EpicKiwi <me@epickiwi.fr>
2018-10-15 07:47:59 -0400

    Storing component context data in a global variable

---
+++ examples/theme-box/ModalCard.js
@@ -0,0 +1,67 @@
+const clemi = require("../../lib/index")
+
+module.exports = clemi.define(class ModalCard extends clemi.Component {
+
+    static get props(){
+        return {
+            withToolbar: {type:Boolean,callback:"enableToolbar"}
+        }
+    }
+
+    enableToolbar(){
+        if(this.props.withToolbar){
+            this.el.toolbar.classList.add("enable")
+        } else {
+            this.el.toolbar.classList.remove("enable")
+        }
+    }
+
+    connectedCallback(){
+        super.connectedCallback()
+        this.context.theme = "light"
+        console.log(`My new theme : ${this.context.theme}`)
+    }
+
+    static get template(){
+        // language=HTML
+        return `
+            <style>             
+                .card {
+                    padding: 15px;
+                    border-radius: 3px;
+                    box-shadow: rgba(0,0,0,0.3) 0 5px 15px;
+                }
+                
+                .title {
+                    margin-bottom: 10px;
+                    color: var(--theme-primary);
+                }
+
+                .toolbar {
+                    margin-top: 10px;
+                    display: none;
+                    
+                    flex-direction: row;
+                    justify-content: flex-end;
+                    align-items: center;
+                }
+                
+                .toolbar.enable {
+                    display: flex;
+                }
+                
+                .toolbar slot::slotted(*) {
+                    display: block;
+                    margin: 0 5px !important;
+                }
+                
+            </style>
+            <div class="card">
+                <div class="title" ><slot name="title"></slot></div>
+                <div class="content"><slot></slot></div>
+                <nav class="toolbar" id="toolbar"><slot name="toolbar"></slot></nav>
+            </div>
+        `
+    }
+
+})
